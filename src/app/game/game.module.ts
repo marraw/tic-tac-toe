74afde519861175a096d68b9a4f32f2d1f5d841b
Marek Rawdanowicz <marek.rawdanowicz@outlook.com>
2020-11-25 21:18:41 +0100

    initial commit

---
+++ src/app/game/game.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
+
+@NgModule({
+  declarations: [BoardComponent, SquareComponent],
+  imports: [],
+})
+export class GameModule {}
